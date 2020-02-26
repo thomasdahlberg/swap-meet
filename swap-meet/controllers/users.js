2e5503fa905e64984139a71f1859793fdaaabafd
Thomas Dahlberg <thomasdahlberg@Thomass-MacBook-Pro.local>
2020-02-25 21:58:13 -0500

    adds token response to new user creation

--- swap-meet/controllers/users.js
+++ swap-meet/controllers/users.js
@@ -1,21 +1,33 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET;
 
 module.exports = {
     signup
 };
 
+function createJWT(user) {
+    return jwt.sign(
+        {user},
+        SECRET,
+        {expiresIn: '24h'}
+    );
+}
+
 async function signup(req, res) {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        adminUser: false,
+        rating: 0,
+        active: true,
+        seeking: []
+    });
     try {
-        const user = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-            adminUser: false,
-            rating: 0,
-            active: true,
-            seeking: []
-        });
-        res.json(user);
+        await user.save();
+        const token = createJWT(user);
+        res.json({ token });
     } catch (error) {
         res.status(400).json(error);
     }
