b750ad5f78954d99ec41a818e98c8758b951b394
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2020-02-29 17:10:50 -0500

    adds map that centers on user's currentLocation

--- swap-meet/src/components/Map/Map.jsx
+++ swap-meet/src/components/Map/Map.jsx
@@ -2,15 +2,39 @@ import { Map, GoogleApiWrapper } from 'google-maps-react';
 import React, {Component} from 'react';
 import styles from './Map.module.css';
 
+
+
 class SwapMap extends Component {
+    constructor() {
+        super();
+        this.state = {
+        };
+      } 
+    
+    componentWillMount() {
+        this.getUserLocation();
+    }
+
+    getUserLocation = () => {
+        navigator.geolocation.getCurrentPosition(this.showPosition)
+    }
+
+    showPosition = position => {
+        this.setState({
+            lat: position.coords.latitude,
+            long: position.coords.longitude,
+        })
+    }
+
 
     render() {
         return(
-            <div>
+            <div onLoad={this.getUserLocation}>
                 <Map
                     google={this.props.google}
-                    zoom={8}
-                    initialCenter={{ lat: 47.444, lng: -122.176}}
+                    zoom={15}
+                    initialCenter={{ lat: this.state.lat, lng: this.state.long }}
+                    centerAroundCurrentLocation={true}
                 />
             </div>
         )
