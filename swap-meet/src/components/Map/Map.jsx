f9a2f62b509b6baf0cf98c223ab2b73591032da6
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2020-05-12 17:02:40 -0400

    adds markers and infowindows to homepage map

--- swap-meet/src/components/Map/Map.jsx
+++ swap-meet/src/components/Map/Map.jsx
@@ -1,47 +1,66 @@
 import React, { Component, createRef } from 'react'
 
 const GOOGLE_MAP_API_KEY = process.env.API_KEY;
+let prevInfoWindow = false;
 
 class GoogleMap extends Component {
   constructor(props) {
     super(props);
-    this.googleMapRef = React.createRef()
+    this.googleMapRef = createRef()
   }
-
   componentDidMount() {
     console.log(this.props.sites[0]);
     const googleMapScript = document.createElement('script')
     googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyA8JacrsSr71qIf9zHzR_6__AbObv3hci8&libraries=places`
     window.document.body.appendChild(googleMapScript)
     googleMapScript.addEventListener('load',() => {
-      this.googleMap = this.createGoogleMap()
-      this.marker = this.createMarker()
+      this.googleMap = this.initMap()
     })
   }
   
 
-  createGoogleMap = () => 
-    new window.google.maps.Map(this.googleMapRef.current, {
-      zoom: 16,
+  initMap = () => {
+    let map = new window.google.maps.Map(this.googleMapRef.current, {
+      zoom: 12,
       center: {
         lat: this.props.lat,
         lng: this.props.lng,
       },
-      disableDefaultUI: true,
-    })  
-
-    createMarker = () =>
-    new window.google.maps.Marker({
-      position: { lat: this.props.sites[0].latitude, lng: this.props.sites[0].longitude },
-      map: this.googleMap,
     })
+    this.setMarkers(map, this.props.sites) 
+  }
+  setMarkers = (map, lib) => {
+    for (let i = 0; i < lib.length; i++) {
+        let place = lib[i];
+        let infoWind = new window.google.maps.InfoWindow({
+            content: ''
+        })
+        let marker = new window.google.maps.Marker({
+            position: {lat: place.latitude, lng: place.longitude},
+            map: map,
+            title: lib[i].siteName
+        })
+        let placeHTML = `<strong>${place.siteName}</strong><br><a class="btn btn-flat btn-small" href="/swapsites/${place.id}">View</a>`;
+        bindInfoWindow(marker, map, infoWind, placeHTML);
+    }
+    function bindInfoWindow(marker, map, infoWind, html) {
+        new window.google.maps.event.addListener(marker, 'click', function() {
+        if(prevInfoWindow){
+            prevInfoWindow.close();
+        }
+        prevInfoWindow = infoWind;
+        infoWind.setContent(html);
+        infoWind.open(map, marker);
+        });
+    } 
+  }
 
   render() {
     return(
       <div
         id="google-map"
         ref={this.googleMapRef}
-        style={{ width: '400px', height: '300px' }}
+        style={{ width: '750px', height: '750px' }}
       />
     )
   }
